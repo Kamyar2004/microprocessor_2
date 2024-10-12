c2bcb3de2ec10fefd505291b22233e56a159daf2
Kamyar <136384387+Kamyar2004@users.noreply.github.com>
2024-10-12 11:07:39 +0330

    Add files via upload

---
+++ experiment_1/microprocessor_2.ino
@@ -0,0 +1,16 @@
+int led1 = 13;
+int led2 = 7;
+
+void setup() {
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+}
+
+void loop() {
+digitalWrite(led1, HIGH);
+digitalWrite(led2, LOW);
+delay(1000);
+digitalWrite(led1, LOW);
+digitalWrite(led2, high);
+delay(1000);
+}
